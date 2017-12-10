baa924375fdde43641e16b0ec9ed49657f71e112
DevHyung <khuphj@gmail.com>
2017-12-10 10:00:02 +0900

    mac -> windows

---
+++ Parser.py
@@ -0,0 +1,13 @@
+"""
+    File name: Parser.py
+    Author: DevHyung
+    Date last modified: 2017/12/09
+    Python Version: 3.6
+    Description : Parsing modules descript this file
+"""
+class Parsing:
+    def __init__(self, lastParsingData, status, logList,memberList):
+        self.lastParsingData = lastParsingData
+        self.status = status
+        self.logList = logList
+        self.memberList = memberList
